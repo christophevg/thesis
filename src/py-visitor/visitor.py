5ce78c84701f085cddf3338e8e8f5793b30d3893
Christophe VG <contact@christophe.vg>
2014-02-10 16:54:22 +0100

    improved Visitor module, added decorator

--- src/py-visitor/visitor.py
+++ src/py-visitor/visitor.py
@@ -3,7 +3,12 @@
 
 # The abstract Visitor and Visitable base-class, providing the redirection
 
+import inspect
+
 class Visitable(object):
+  """
+  Baseclass for visitable classes
+  """
   def accept(self, visitor):
     class_name  = self.__class__.__name__
     method_name = "handle_" + class_name
@@ -13,7 +18,40 @@ class Visitable(object):
       print visitor.__class__.__name__ + " doesn't handle " + class_name
 
 class Visitor(object):
+  """
+  Baseclass for visitors.
+  """
   pass
+
+class visitor_for(object):
+  """
+  Decorator @visit(superclass) makes a decorated class a visitor for all classes in
+  the module of the decorated class. The visitable classes can be limited to
+  a common superclass.
+  """
+  def __init__(self, super=None):
+    self.super = super
+
+  def __call__(self, visitor):
+    # make the visitor a subclass of Visitor
+    visitor = type(visitor.__name__, (Visitor,), dict(visitor.__dict__))
+
+    # retrieve calling module and its classes
+    frame   = inspect.stack()[1]
+    module  = inspect.getmodule(frame[0])
+    classes = inspect.getmembers(module, inspect.isclass)
+
+    # dynamically adding all Stmt subclasses tot the visitor
+    def NotImplemented(name):
+      def dummy(self, *args):
+        print name + " is not implemented in " + self.__class__.__name__
+      return dummy
+
+    for name, clazz in classes:
+      if super == None or issubclass(clazz, self.super):
+        setattr(visitor, "handle_" + name, NotImplemented("handle_" + name))
+
+    return visitor
 
 # The Code Model with an unimplemented CodeVisitor
 
@@ -30,25 +68,10 @@ class PrintStmt(Stmt):
   def __init__(self, data):
     self.data = data
 
-class ReturnStmt(Stmt):
-  pass
+class ReturnStmt(Stmt): pass
 
-class CodeVisitor(Visitor):
-  pass
-
-import sys
-import inspect
-
-# dynamically adding all Stmt subclasses tot the visitor
-def NotImplemented(name):
-  def dummy(self, *args):
-    print name + " is not implemented in " + self.__class__.__name__
-  return dummy
-
-classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
-for name, clazz in classes:
-  if issubclass(clazz, Stmt):
-    setattr(CodeVisitor, "handle_" + name, NotImplemented("handle_" + name))
+@visitor_for(Stmt)
+class CodeVisitor(): pass
 
 # A first CodeVisitor, interpreting
 
